6897560b8d004c9acb337c675b613e697283132e
SHAHADAT HOSSEN SHUVO <2027shuvo@gmail.com>
2025-12-20 23:57:27 +0600

    Day-6

---
+++ Week-1/Day-6/stack scope.py
@@ -0,0 +1,9 @@
+def calculate_area (radius: float) -> float:
+    """ Returns area of circle. Inputs float."""
+
+    if radius < 0:
+        return 0
+    return 3.14 * (radius ** 2)
+
+r = 5
+print (calculate_area(r))
